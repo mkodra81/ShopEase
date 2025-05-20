3b44a1e5dac2c29dd6e21f2fbc0e20d366ecb8bd
Mateo <kodramateo4@gmail.com>
2025-05-20 15:20:59 +0200

    server fix

--- backend/server.js
+++ backend/server.js
@@ -7,27 +7,23 @@ import orderRouter from "./routes/orders.route.js";
 import cors from "cors";
 import path from "path";
 import cookieParser from "cookie-parser";
-import helmet from "helmet";
 
 const app = express();
 
 config();
-const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = "http://localhost:5173";
+const PORT = process.env.PORT;
 
-app.use(helmet()); //Security
 app.use(cookieParser());
 
 app.use(
   cors({
-    origin: FRONTEND_URL,
-    credentials: true,
-    exposedHeaders: ["set-cookie"],
+    origin: "http://localhost:5173", // Replace with your frontend's URL
+    credentials: true, 
   })
 );
 
-app.use(express.json({ limit: "100mb" }));
-app.use("/images", express.static(path.resolve("images")));
+app.use(express.json({ limit: "1000mb", extended: true }));
+app.use("/images", express.static(path.join(path.resolve(), "images")));
 
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
@@ -35,5 +31,5 @@ app.use("/api/orders", orderRouter);
 
 app.listen(PORT, () => {
   connectDb();
-  console.log(`Server started at http://localhost:${PORT}`);
+  console.log("Server started at http://localhost:" + PORT);
 });
