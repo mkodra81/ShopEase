3e5ed0c8269959e0fa4b5bf81527e2a414ab560a
Mateo <kodramateo4@gmail.com>
2025-10-28 23:18:30 +0100

    login

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,12 @@ config();
 
 app.use(cookieParser());
 
-app.use(cors())
+const allowedOrigin = "https://shop-ease-omega-steel.vercel.app";
+
+app.options("*", cors({
+  origin: allowedOrigin,
+  credentials: true
+}));
 
 app.use(express.json({ limit: "1000mb", extended: true }));
 app.use("/images", express.static(path.join(path.resolve(), "images")));
